fc12042c99ae4da04355fe18ec784d6cb089c729
EduardoCampos0407 <eduardobalc@al.insper.edu.br>
2026-05-01 00:32:17 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,12 @@
+import random
+
+def rolar_dados(n):
+    dados_r = []
+    
+    for i in range(n):
+        dado = random.randint(1, 6)
+        dados_r.append(dado)
+    
+    return dados_r
+
 
