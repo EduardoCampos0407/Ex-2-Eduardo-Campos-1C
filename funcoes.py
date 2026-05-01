c366a0809ceb6f4ea3ec84607afc961e1e420887
EduardoCampos0407 <eduardobalc@al.insper.edu.br>
2026-05-01 13:38:04 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,12 +1,29 @@
 import random
 
 def rolar_dados(n):
-    dados_r = []
+    dados_rolados = []
     
     for i in range(n):
         dado = random.randint(1, 6)
-        dados_r.append(dado)
+        dados_rolados.append(dado)
     
-    return dados_r
+    return dados_rolados
 
 
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    
+    dado = dados_rolados[dado_para_guardar]
+    
+    dados_no_estoque.append(dado)
+    
+    dados_rolados.pop(dado_para_guardar)
+    
+    return [dados_rolados, dados_no_estoque]
+
+def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
+    
+    dado = dados_no_estoque.pop(dado_para_remover)
+    
+    dados_rolados.append(dado)
+    
+    return [dados_rolados, dados_no_estoque]
