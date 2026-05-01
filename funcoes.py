b630c1976f22bfdd4ff0b7cfb178b07274b657ff
EduardoCampos0407 <eduardobalc@al.insper.edu.br>
2026-05-01 14:11:37 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -28,3 +28,21 @@ def calcula_pontos_regra_simples(dados):
                 soma += dado 
         resultado[face] = soma
     return resultado
+
+def calcula_pontos_soma(dados):
+    soma = 0
+    for dado in dados:
+        soma += dado
+    return soma
+
+def calcula_pontos_sequencia_baixa(dados):
+    if 1 in dados and 2 in dados and 3 in dados and 4 in dados:
+        return 15
+    
+    if 2 in dados and 3 in dados and 4 in dados and 5 in dados:
+        return 15
+    
+    if 3 in dados and 4 in dados and 5 in dados and 6 in dados:
+        return 15
+    
+    return 0
